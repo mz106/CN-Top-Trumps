0cc8a61b879d8dc6f9f45a9642ad3ecea4e65522
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2021-07-20 12:11:42 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 const prompt = require("prompt-sync")({sigint: true}); 
 
 let nameOne = prompt('Player one, enter your name: ');
@@ -78,6 +79,10 @@ class Warrior extends Character {
         this._intelligence = 3;
         this.mana = 0;
     }
+
+    get type() {
+        return this._type;
+    }
 };
 
 class Wizard extends Character {
@@ -89,6 +94,10 @@ class Wizard extends Character {
         this._agility = 8;
         this._intelligence = 10;
         this.mana = 10;
+    }
+
+    get type() {
+        return this._type;
     }
 };
 
@@ -102,6 +111,10 @@ class Scout extends Character {
         this._intelligence = 10;
         this.mana = 3;
     }
+
+    get type() {
+        return this._type;
+    }
 };
 
 class Knight extends Character {
@@ -114,6 +127,10 @@ class Knight extends Character {
         this._intelligence = 5;
         this.mana = 0;
     }
+
+    get type() {
+        return this._type;
+    }
 };
 
 class Troubadour extends Character {
@@ -125,6 +142,74 @@ class Troubadour extends Character {
         this._agility = 10;
         this._intelligence = 6;
         this.mana = 5;
+    }
+
+    get type() {
+        return this._type;
+    }
+};
+
+class Archer extends Character {
+    constructor(type, health, strength, agility, intelligence, mana) {
+        super(health, strength, agility, intelligence, mana);
+        this._type = 'Archer';
+        this._health = 5;
+        this._strength = 3;
+        this._agility = 10;
+        this._intelligence = 8;
+        this.mana = 1;
+    }
+
+    get type() {
+        return this._type;
+    }
+};
+
+class Blacksmith extends Character {
+    constructor(type, health, strength, agility, intelligence, mana) {
+        super(health, strength, agility, intelligence, mana);
+        this._type = 'Blacksmith';
+        this._health = 8;
+        this._strength = 10;
+        this._agility = 3;
+        this._intelligence = 7;
+        this.mana = 2;
+    }
+
+    get type() {
+        return this._type;
+    }
+};
+
+class Pistoler extends Character {
+    constructor(type, health, strength, agility, intelligence, mana) {
+        super(health, strength, agility, intelligence, mana);
+        this._type = 'Pistoler';
+        this._health = 4;
+        this._strength = 15;
+        this._agility = 4;
+        this._intelligence = 5;
+        this.mana = 0;
+    }
+
+    get type() {
+        return this._type;
+    }
+};
+
+class Pikeman extends Character {
+    constructor(type, health, strength, agility, intelligence, mana) {
+        super(health, strength, agility, intelligence, mana);
+        this._type = 'Pikeman';
+        this._health = 3;
+        this._strength = 7;
+        this._agility = 11;
+        this._intelligence = 2;
+        this.mana = 1;
+    }
+
+    get type() {
+        return this._type;
     }
 };
 
@@ -147,7 +232,7 @@ class Player {
     }
 
     createCard() {
-        let ranNum = Math.floor(Math.random() * 4);
+        let ranNum = Math.floor(Math.random() * 9);
         if (ranNum === 0) {
             let newWarrior = new Warrior();
             return newWarrior;
@@ -163,11 +248,22 @@ class Player {
         } else if(ranNum === 4) {
             let newTroubadour = new Troubadour();
             return newTroubadour;
-        } 
+        } else if (ranNum === 5) {
+            let newArcher = new Archer();
+            return newArcher;
+        } else if (ranNum === 6) {
+            let newBlackSmith = new Blacksmith();
+            return newBlackSmith;
+        } else if (ranNum === 7) {
+            let newPistoler = new Pistoler();
+            return newPistoler;
+        } else if (ranNum === 8) {
+            let newPikeman = new Pikeman();
+            return newPikeman;
+        }
 
     }
 };
-
 
 const playerOne = new Player(nameOne, true);
 const playerTwo = new Player(nameTwo, false);
@@ -175,14 +271,46 @@ const playerTwo = new Player(nameTwo, false);
 function play(par1, par2) {
     let cardOne = par1.createCard();
     let cardTwo = par2.createCard();
-    console.log(cardOne)
-    let keysOne = Object.keys(cardOne);
-    let valuesOne = Object.values(cardOne)
-    console.log(valuesOne)
-    let ranNumOne = Math.floor(Math.random() * 5);
-    console.log(cardOne[valuesOne[ranNumOne]], cardOne[keysOne[ranNumOne]], ranNumOne);
     
+    const propArr = ['health', 'strength', 'agility', 'intelligence', 'mana'];
+    let prop;
+    const ranNum = Math.floor(Math.random() * 4);
+    for (let i = 0; i < propArr.length; i++) {
+        if (ranNum === i) {
+            prop = propArr[i];
+        }
+    }
+    propOne = cardOne[prop];
+    propTwo = cardTwo[prop];
+
+    console.log(cardOne.type, prop, propOne)
+    console.log(cardTwo.type, prop, propTwo)
+
+    if (propOne > propTwo) {
+        console.log(`${playerOne.name} wins this round!`)
+    } else if (propTwo > propOne) {
+        console.log(`${playerTwo.name} wins this round!`)
+    } else if (propOne === propTwo) {
+        console.log(`'It's a draw!`)
+    }
+
+    // let keysOne = Object.keys(cardOne);
+    
+    // let valuesOne = Object.values(cardOne)
+    // let entriesOne = Object.entries(cardOne);
+    // console.log(entriesOne)
+    // let ranNumOne = Math.floor(Math.random() * 5);
+    // // console.log(cardOne[keysOne[ranNumOne]]);
+    
+    // let keysTwo = Object.keys(cardTwo);
+    // let entriesTwo = Object.entries(cardTwo);
+    // let valuesTwo = Object.values(cardTwo)
+    // // console.log(keysOne, valuesOne, cardOne[keysOne[ranNumOne]], keysTwo, valuesTwo, cardTwo[keysTwo[ranNumOne]], `This is the ranNum: ${ranNumOne}`);
+    // console.log(cardOne[ranNumOne])
 };
 
 play(playerOne, playerTwo);
+
+
+
 
